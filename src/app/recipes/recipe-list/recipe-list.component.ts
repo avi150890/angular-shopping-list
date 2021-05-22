2ef2af068d32b34f73c145fc9365b7d58de53f31
avi150890 <avinash15sharma@gmail.com>
2021-05-23 02:20:43 +0530

    further changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe1', 'This is simply a Test1', 'https://cdn.pixabay.com/photo/2017/10/16/09/01/hamburger-2856548_960_720.jpg'),
+    new Recipe('A test Recipe2', 'This is simply a Test2', 'https://cdn.pixabay.com/photo/2016/01/14/17/46/eat-1140371_960_720.jpg')
 
+  ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
